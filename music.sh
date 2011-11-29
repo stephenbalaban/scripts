7d5674fa4879b2e5221488cd4b2c078f7db3764b
stephen@stephenbalaban.com <mail@stephenbalaban.com>
2011-11-29 08:30:53 -0500

    added     markdown2pdf_full - full pages on the markdown2pdf converter from pandoc     added print, a light wrapping on lpr

--- music.sh
+++ music.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+musicdir="/home/sabalaba/音乐/";
 if [[ $1 ]]; then
     tunes=$1;
 else
     tunes="trance";
 fi;
-cvlc --qt-minimal-view --equalizer-preset dance --no-qt-error-dialogs ~/音乐/$tunes.pls
+cvlc --qt-minimal-view --equalizer-preset dance --no-qt-error-dialogs $musicdir$tunes.pls
